35b19cc60b2f4016ba2ba0505174d10bd9c9d467
Justyn Meza <justinmesa2013@gmail.com>
2021-12-04 19:00:59 -0500

    Update carousel 1

--- Code Project/Js/Main.js
+++ Code Project/Js/Main.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const carouselElements = document.querySelectorAll('.carousel');
     M.Carousel.init(carouselElements, {
-        duration: 150
-    });
+        duration: 150,
+        dist: -80,
+        shift: 5,
+        padding: 50,
+        numVisible: 5,
+        indicators: true,
+        noWrap: false
+    });    
+});
+
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+    overlay = document.getElementById('overlay'),
+    popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+    overlay.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(){
+    overlay.classList.remove('active');
 });
